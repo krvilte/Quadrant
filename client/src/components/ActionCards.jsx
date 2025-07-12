7d8ddd29308b7f2ed723f5b680b54926e2f9493b
Vishal Kumar Sah <thevishalkumar25@gmail.com>
2025-07-12 23:49:01 +0530

    quadrant view setup

--- client/src/components/ActionCards.jsx
+++ client/src/components/ActionCards.jsx
@@ -1,39 +1,45 @@
 import React from "react";
 import Quadrant from "./Quadrant";
-import { CircleCheck } from "lucide-react";
+import { Check } from "lucide-react";
 
 const quadrants = [
   {
-    key: "do",
+    id: "do",
     title: "Do First",
-    icon: <CircleCheck className="w-5 h-5 text-green-600" />,
+    icon: <Check className="w-5 h-5 rounded bg-green-400 text-white" />,
     color: "text-green-600",
   },
   {
-    key: "decide",
+    id: "decide",
     title: "Decide",
-    icon: <CircleCheck className="w-5 h-5 text-yellow-500" />,
+    icon: <Check className="w-5 h-5 rounded bg-yellow-400 text-white" />,
     color: "text-yellow-500",
   },
   {
-    key: "delegate",
+    id: "delegate",
     title: "Delegate",
-    icon: <CircleCheck className="w-5 h-5 text-blue-600" />,
+    icon: <Check className="w-5 h-5 rounded bg-blue-400 text-white" />,
     color: "text-blue-600",
   },
   {
-    key: "delete",
+    id: "delete",
     title: "Delete",
-    icon: <CircleCheck className="w-5 h-5 text-red-600" />,
+    icon: <Check className="w-5 h-5 rounded bg-red-400 text-white" />,
     color: "text-red-600",
   },
 ];
 
 function ActionCards() {
   return (
-    <div className="grid md:grid-cols-4 gap-6">
+    <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4">
       {quadrants.map((q) => (
-        <Quadrant key={q.key} title={q.title} icon={q.icon} color={q.color} />
+        <Quadrant
+          key={q.id}
+          status={q.id}
+          title={q.title}
+          icon={q.icon}
+          color={q.color}
+        />
       ))}
     </div>
   );
--- client/src/components/Quadrant.jsx
+++ client/src/components/Quadrant.jsx
@@ -1,16 +1,34 @@
 import React from "react";
+import { Plus } from "lucide-react";
 
-function Quadrant({ title, icon, color }) {
+function Quadrant({ status, title, icon }) {
   return (
-    <div className="p-6 bg-white rounded-md shadow-xs border border-gray-200 hover:shadow-md">
-      <div className="flex items-center gap-2 mb-4">
-        {icon}
-        <h2 className={`text-lg font-semibold ${color}`}>{title}</h2>
-      </div>
+    <div className="p-4 bg-white border border-gray-100 rounded-lg">
+      <div className="flex flex-col items-start mb-6">
+        {/* Header */}
+        <div className="flex gap-2 items-center w-full py-2 mb-4">
+          {icon}
+          <h2
+            className={`text-md font-semibold uppercase text-gray-600 select-none`}
+          >
+            {title}
+          </h2>
+        </div>
 
-      <p className="text-sm text-gray-400 italic">
-        (Tasks will appear here...)
-      </p>
+        {/* Task Placeholder */}
+        <div className="mb-2 w-full">
+          <p className="border border-gray-100 p-2 rounded">lorem ipsum</p>
+        </div>
+
+        {/* Add Task */}
+        <button
+          className="hover:underline hover:text-blue-400"
+          name={status}
+          onClick={(e) => console.log(e.target.name)}
+        >
+          + Add Task
+        </button>
+      </div>
     </div>
   );
 }
