9c57939176c4e3c7fd78724badcbffdc2a0b81e4
Vishal Kumar Sah <thevishalkumar25@gmail.com>
2025-07-26 23:49:15 +0530

    implement user register controller

--- server/src/controllers/user.controllers.js
+++ server/src/controllers/user.controllers.js
@@ -1,10 +1,43 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiResponse.js";
+import User from "../models/user.model.js";
+
 // @desc Register User
 // @route POST "/api/v1/users/register"
 // @access public
+export const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, username, email, password } = req.body;
 
-export const registerUser = (req, res) => {
-  res.send("Register user");
-};
+  if (!fullName && !username && !email && !password)
+    throw new ApiError(400, "All fields are required");
+
+  // Check existing user
+  const existingUser = await User.findOne({ $or: [{ username, email }] });
+  if (existingUser)
+    throw new ApiError(409, "User with email or password already exists");
+
+  // Create user
+  const user = await User.create({
+    fullName,
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  // Find registered user
+  const registeredUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  // Validate registered user
+  if (!registeredUser) throw new ApiError(500, "Error while registering user");
+
+  // Success response
+  return res
+    .status(201)
+    .json(new ApiResponse(200, "User registered successfully", registeredUser));
+});
 
 // @desc Login User
 // @route POST "/api/v1/users/login"
